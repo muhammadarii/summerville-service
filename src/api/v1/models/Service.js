19097657c7d13be51cd790ca70d2a7206b2d09b6
ciyek <aripurnomo606@gmail.com>
2025-05-19 13:02:35 +0700

    feat: Implement service management API with CRUD operations

---
+++ src/api/v1/models/Service.js
@@ -0,0 +1,23 @@
+const mogoose = require("mongoose");
+
+const serviceSchema = new mogoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mogoose.model("Service", serviceSchema);
